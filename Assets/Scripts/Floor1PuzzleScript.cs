23178589241d7f54e3701826617b4186cd3d97da
NanosHere <71567368+NanosHere@users.noreply.github.com>
2024-01-15 23:00:56 -0500

    changes

--- Assets/Scripts/Floor1PuzzleScript.cs
+++ Assets/Scripts/Floor1PuzzleScript.cs
@@ -6,20 +6,33 @@ using UnityEngine.XR.Content.Interaction;
 
 public class Floor1PuzzleScript : MonoBehaviour
 {
+    [Header("Lights")]
+    [Space(40, order = 2)]
+    public List<GameObject> Lights;
+
     [Header("Puzzle 1")]
     [Space(40, order = 2)]
     public int correctNumber;
 
     [Header("Puzzle 2")]
+    bool GeneratorActive = false;
+
     [Space(40, order = 2)]
-   
+    [Header("Knob")]
+    private int activeGenerator = 1;
+
 
     [Header("Knob")]
     [Tooltip("Value from 0-8")]
     public float correctKnob1;
     public float correctKnob2;
-    float currentKnob1;
-    float currentKnob2;
+
+    private float Resetknob1;
+    private float Resetknob2;
+
+    private float currentKnob1;
+    private float currentKnob2;
+
 
     public GameObject knob1;
     public GameObject knob2;
@@ -31,12 +44,17 @@ public class Floor1PuzzleScript : MonoBehaviour
     [Range(0f, 1f)] public float correctSlider2;
     [Range(0f, 1f)] public float correctSlider3;
     [Range(0f, 1f)] public float correctSlider4;
-    
 
-    float currentSlider1;
-    float currentSlider2;
-    float currentSlider3;
-    float currentSlider4;
+
+    private float ResetSlider1;
+    private float ResetSlider2;
+    private float ResetSlider3;
+    private float ResetSlider4;
+
+    private float currentSlider1;
+    private float currentSlider2;
+    private float currentSlider3;
+    private float currentSlider4;
     
     public GameObject Slider1;
     public GameObject Slider2;
@@ -51,6 +69,9 @@ public class Floor1PuzzleScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        GeneratorOff();
+        StartUpGenerator();
+        GetReset();
         
     }
 
@@ -60,24 +81,140 @@ public class Floor1PuzzleScript : MonoBehaviour
         
     }
 
+
+    // generator startup function;
+    public void StartUpGenerator()
+    {
+        currentSlider1 = Slider1.GetComponent<XRSlider>().value;
+        
+        currentSlider2 = Slider2.GetComponent<XRSlider>().value;
+        
+        currentSlider3 = Slider3.GetComponent<XRSlider>().value;
+        
+        currentSlider4 = Slider4.GetComponent<XRSlider>().value;
+       
+        currentKnob1 = knob1.GetComponent<XRKnob>().value;
+        
+        currentKnob2 = knob2.GetComponent<XRKnob>().value;
+        
+    }
+
+    // reset values of generator
+    public void ResetGenerator()
+    {
+        Slider1.GetComponent<XRSlider>().value = ResetSlider1;
+        Slider2.GetComponent<XRSlider>().value = ResetSlider2;
+        Slider3.GetComponent<XRSlider>().value = ResetSlider3;
+        Slider4.GetComponent<XRSlider>().value = ResetSlider4;
+        knob1.GetComponent<XRKnob>().value = Resetknob1;
+        knob2.GetComponent<XRKnob>().value = Resetknob2;
+
+    }
+    public void GetReset()
+    {
+        ResetSlider1 = currentSlider1;
+        ResetSlider2 = currentSlider2;
+        ResetSlider3 = currentSlider3;
+        ResetSlider4 = currentSlider4;
+        Resetknob1 = currentKnob1;
+        currentKnob1 = Resetknob1;
+    }
+
     public void GetSlider()
     {
        currentSlider1 =  Slider1.GetComponent<XRSlider>().value;
+       currentSlider2 = Slider2.GetComponent<XRSlider>().value;
+        currentSlider3 = Slider3.GetComponent<XRSlider>().value;
+        currentSlider4 = Slider4.GetComponent<XRSlider>().value;
         Debug.Log(currentSlider1);
     }
     public void getLever(int num)
     {
 
     }
+    public void changeKnob()
+    {
+        currentKnob1 = knob1.GetComponent<XRKnob>().value;
+        currentKnob2 = knob2.GetComponent<XRKnob>().value;
+    }
     public void SwitchGenerator(int num)
     {
-
+        activeGenerator = num;
     }
-    public void checkAnswer()
+    public void CheckAnswer()
     {
+        Debug.Log(activeGenerator);
+        Debug.Log(currentKnob1);
+        Debug.Log(currentKnob2);
+        Debug.Log(currentSlider1);
+        Debug.Log(currentSlider2);
+        Debug.Log(currentSlider3);
+        Debug.Log(currentSlider4);
+
+        if (activeGenerator == 0)
+        {
+            Debug.Log(activeGenerator);
+            if (currentKnob1 == correctKnob1 && currentKnob2 == correctKnob2)
+            {
+                Debug.Log(currentKnob1);
+                Debug.Log(currentKnob2);
+                if ( currentSlider1 > (correctSlider1 - 0.10f) && currentSlider1 < (correctSlider1 + 0.10f) )
+                {
+                    Debug.Log(currentSlider1);
+                    if (currentSlider2 > (correctSlider2 - 0.10f) && currentSlider2 < (correctSlider2 + 0.10f))
+                    {
+                        Debug.Log(currentSlider2);
+                        if (currentSlider3 > (correctSlider3 - 0.10f) && currentSlider3 < (correctSlider3 + 0.10f))
+                        {
+                            Debug.Log(currentSlider3);
+                            if (currentSlider4 > (correctSlider4 - 0.10f) && currentSlider4 < (correctSlider4 + 0.10f))
+                            {
+                                Debug.Log(currentSlider4);
+                                Debug.Log("generator On");
+
+                                GeneratorOn();
+                                GeneratorActive = true;
+                            }
+
+
+
+
+                        }
+
+
+
+
+                    }
+
+
+
+                }
+
+            }
+        }
+
+        Debug.Log("generator fail");
+        if(GeneratorActive == false) { 
+        ResetGenerator();
+        StartUpGenerator();
+        }
 
     }
 
-    
+    public void GeneratorOff()
+    {
+        foreach(GameObject lights in Lights)
+        {
+            lights.gameObject.SetActive(false) ;
+        }
+    }
+    public void GeneratorOn()
+    {
+        foreach (GameObject lights in Lights)
+        {
+            lights.gameObject.SetActive(true);
+        }
+    }
+
 
 }
