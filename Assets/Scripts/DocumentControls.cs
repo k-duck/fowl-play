279f28b372920c2bc31fe903b06f9171d3103709
Ferretex <thomaspvanderwyst@gmail.com>
2024-01-17 22:03:07 -0500

    Added email viewing functionality

---
+++ Assets/Scripts/DocumentControls.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DocumentControls : MonoBehaviour
+{
+    public GameObject documentsWindow;
+    public List<GameObject> documents;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void openDocsWindow()
+    {
+        if (documentsWindow.activeSelf)
+        {
+            documentsWindow.SetActive(false);
+        } else
+        {
+            documentsWindow.SetActive(true);
+        }
+    }
+
+    public void openDocument(int docID)
+    {
+        closeDocs();
+        documents[docID].SetActive(true);
+    }
+
+    void closeDocs()
+    {
+        foreach (GameObject document in documents)
+        {
+            document.SetActive(false);
+        }
+    }
+}
