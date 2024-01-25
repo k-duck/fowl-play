a893dc71b9dc68ac1a8680b43d2ef0f18c74a750
Ferretex <thomaspvanderwyst@gmail.com>
2024-01-25 13:06:30 -0500

    Added draggable windows with boundries

--- Assets/Scripts/DocumentControls.cs
+++ Assets/Scripts/DocumentControls.cs
@@ -1,22 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class DocumentControls : MonoBehaviour
+public class DocumentControls : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
     public GameObject documentsWindow;
     public List<GameObject> documents;
-    
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField]  private Canvas canvas;
+    [SerializeField] private RectTransform dragControls;
+    private float maxDragX, maxDragY, minDragX, minDragY;
+
+    private void Start()
     {
-        
+        maxDragX = canvas.GetComponentInChildren<RectTransform>().rect.xMax;
+        maxDragY = canvas.GetComponentInChildren<RectTransform>().rect.yMax;
+        minDragX = canvas.GetComponentInChildren<RectTransform>().rect.xMin;
+        minDragY = canvas.GetComponentInChildren<RectTransform>().rect.yMin;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        
+        Debug.Log("OnBeginDrag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag");    
+        dragControls.anchoredPosition += eventData.delta / canvas.scaleFactor;
+        KeepInBounds();
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+        KeepInBounds();
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerDown");
+    }
+
+    public void KeepInBounds()
+    {
+        Debug.Log("Email Pos: " + dragControls.anchoredPosition);
+        if (dragControls.anchoredPosition.x - dragControls.rect.xMax + 50 > maxDragX)
+            dragControls.anchoredPosition = new Vector2(maxDragX + dragControls.rect.xMax - 50, dragControls.anchoredPosition.y);
+        if (dragControls.anchoredPosition.y - dragControls.rect.yMax + 50 > maxDragY)
+            dragControls.anchoredPosition = new Vector2(dragControls.anchoredPosition.x, maxDragY + dragControls.rect.yMax - 50);
+        if (dragControls.anchoredPosition.x - dragControls.rect.xMin - 50 < minDragX)
+            dragControls.anchoredPosition = new Vector2(minDragX + dragControls.rect.xMin + 50, dragControls.anchoredPosition.y);
+        if (dragControls.anchoredPosition.y - dragControls.rect.yMin - 50 < minDragY)
+            dragControls.anchoredPosition = new Vector2(dragControls.anchoredPosition.x, minDragY + dragControls.rect.yMin + 50);
     }
 
     public void openDocsWindow()
