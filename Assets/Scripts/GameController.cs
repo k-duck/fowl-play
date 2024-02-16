bb113e8253544b29d0d010b8efd100acb1b9ddb8
Elis Joynes <smj.joynes@gmail.com>
2024-02-16 03:56:28 -0500

    Trying desperately to figure out the settings

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,255 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.InputSystem.XR;
+using UnityEngine.UI;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
+using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
+
+public class GameController : MonoBehaviour
+{
+    private UnityEngine.XR.InputDevice deviceRight;
+    private UnityEngine.XR.InputDevice deviceLeft;
+
+    private ActionBasedControllerManager controllerLeft;
+    private ActionBasedControllerManager controllerRight;
+    private GameObject tunnelControl;
+
+    private bool moveType = false; // 0 = Smooth   1 = Teleport
+    private bool turnType = false; // 0 = Smooth   1 = Snap
+    private float turnStrength = 0.5f; // Turn value (degrees for snap turn, speed for smooth turn)
+    private bool handedness = false; // 0 = Right   1 = Left
+    private bool tunneling = false; // 0 = Off   1 = On
+    private float tunnelStrength = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controllerLeft = GameObject.Find("Left Controller").GetComponent<ActionBasedControllerManager>();
+        controllerRight = GameObject.Find("Right Controller").GetComponent<ActionBasedControllerManager>();
+
+        if (controllerLeft == null || controllerRight == null)
+        {
+            if (moveType == false && handedness == false) // Smooth move & Right handed (Smooth locomotion control on Left hand and turn on right)
+            {
+                controllerLeft.smoothMotionEnabled = true;
+                controllerRight.smoothMotionEnabled = false;
+                controllerRight.transform.Find("Teleport Interactor").gameObject.SetActive(false);
+                controllerLeft.transform.Find("Teleport Interactor").gameObject.SetActive(false);
+
+                if (turnType == false)
+                {
+                    controllerRight.smoothTurnEnabled = true;
+                }
+                else
+                {
+                    controllerRight.smoothTurnEnabled = false;
+                }
+            }
+
+            if (moveType != false && handedness == false) // Teleport move & Right handed (Teleport locomotion control on Left hand and turn on right)
+            {
+                controllerLeft.smoothMotionEnabled = false;
+                controllerRight.smoothMotionEnabled = false;
+                controllerRight.transform.Find("Teleport Interactor").gameObject.SetActive(false);
+                controllerLeft.transform.Find("Teleport Interactor").gameObject.SetActive(true);
+
+                if (turnType != false)
+                {
+                    controllerRight.smoothTurnEnabled = true;
+                }
+                else
+                {
+                    controllerRight.smoothTurnEnabled = false;
+                }
+            }
+
+            if (moveType == false && handedness != false) // Smooth move & Left handed (Smooth locomotion control on Right hand and turn on right)
+            {
+                controllerLeft.smoothMotionEnabled = false;
+                controllerRight.smoothMotionEnabled = true;
+                controllerRight.transform.Find("Teleport Interactor").gameObject.SetActive(false);
+                controllerLeft.transform.Find("Teleport Interactor").gameObject.SetActive(false);
+
+                if (turnType == false)
+                {
+                    controllerLeft.smoothTurnEnabled = true;
+                }
+                else
+                {
+                    controllerLeft.smoothTurnEnabled = false;
+                }
+            }
+
+            if (moveType != false && handedness != false) // Teleport move & Left handed (Teleport locomotion control on Right hand and turn on right)
+            {
+                controllerLeft.smoothMotionEnabled = false;
+                controllerRight.smoothMotionEnabled = false;
+                controllerRight.transform.Find("Teleport Interactor").gameObject.SetActive(true);
+                controllerLeft.transform.Find("Teleport Interactor").gameObject.SetActive(false);
+
+                if (turnType != false)
+                {
+                    controllerLeft.smoothTurnEnabled = true;
+                }
+                else
+                {
+                    controllerLeft.smoothTurnEnabled = false;
+                }
+            }
+
+            Debug.Log("Left Controller: " + controllerLeft);
+            Debug.Log("Right Controller: " + controllerRight);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        var inputDevices = new List<UnityEngine.XR.InputDevice>();
+        UnityEngine.XR.InputDevices.GetDevices(inputDevices);
+
+        if (inputDevices.Count > 2)
+        {
+            Debug.Log("Found One");
+        }
+        else
+        {
+            foreach (var device in inputDevices)
+            {
+                Debug.Log("Input Device: " + device.name + "Count: " + inputDevices.Count);
+            }
+        }*/
+
+        var leftHandDevices = new List<UnityEngine.XR.InputDevice>();
+        UnityEngine.XR.InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.LeftHand, leftHandDevices);
+
+        var rightHandDevices = new List<UnityEngine.XR.InputDevice>();
+        UnityEngine.XR.InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.RightHand, rightHandDevices);
+
+        if (leftHandDevices.Count == 1)
+        {
+            deviceLeft = leftHandDevices[0];
+            //Debug.Log(string.Format("Device name '{0}' with role '{1}'", deviceLeft.name, deviceLeft.role.ToString()));
+        }
+        else if (leftHandDevices.Count > 1)
+        {
+            Debug.Log("Found more than one left hand!");
+        }
+
+        if (rightHandDevices.Count == 1)
+        {
+            deviceRight = rightHandDevices[0];
+            //Debug.Log(string.Format("Device name '{0}' with role '{1}'", deviceRight.name, deviceRight.role.ToString()));
+        }
+        else if (rightHandDevices.Count > 1)
+        {
+            Debug.Log("Found more than one right hand!");
+        }
+
+
+        UpdateMove();
+        UpdateTurn();
+        UpdateTunneling();
+
+    }
+
+    public void SetMove(TMP_Dropdown state) // 0 = Smooth   1 = Teleport
+    {
+        if (state.value == 0)
+        {
+            moveType = false;
+        }
+        else
+        {
+            moveType = true;
+        }
+    }
+
+    public void SetTurn(TMP_Dropdown state) // 0 = Smooth   1 = Snap
+    {
+        if (state.value == 0)
+        {
+            turnType = false;
+        }
+        else
+        {
+            turnType = true;
+        }
+    }
+
+    public void SetTurnVal(float value)
+    {
+        turnStrength = value;
+    }
+
+    public void SetTunneling(Toggle state) // 0 = Off   1 = On
+    {
+        tunneling = state.isOn;
+    }
+
+    public void SetTunnelingVal(Slider state)
+    {
+        tunnelStrength = state.value;
+    }
+    public void SetHandedness(TMP_Dropdown state) // 0 = Right   1 = Left
+    {
+        //handedness = hand;
+        if (state.value == 0)
+        {
+            handedness = false;
+        }
+        else
+        {
+            handedness = true;
+        }
+    }
+
+    void UpdateMove()
+    {
+        if (handedness)
+        {
+            controllerLeft.smoothMotionEnabled = false;
+            controllerRight.smoothMotionEnabled = !moveType;
+        }
+        else
+        {
+            controllerRight.smoothMotionEnabled = false;
+            controllerLeft.smoothMotionEnabled = !moveType;
+        }
+
+        Debug.Log("Controller: " + handedness);
+    }
+
+    void UpdateTurn()
+    {
+        if (handedness)
+        {
+            controllerLeft.smoothTurnEnabled = !turnType;
+            controllerRight.smoothTurnEnabled = false;
+        }
+        else
+        {
+            controllerRight.smoothTurnEnabled = !turnType;
+            controllerLeft.smoothTurnEnabled = false;
+        }
+    }
+
+    void UpdateTunneling()
+    {
+        tunnelControl = GameObject.Find("TunnelingVignette");
+
+        tunnelControl.SetActive(tunneling);
+
+        //tunnelControl.GetComponent<TunnelingVignetteController>().apperatureSize = tunnelStrength;
+    }
+
+}
+
+
--- Assets/Scripts/StartSceneScript.cs
+++ Assets/Scripts/StartSceneScript.cs
@@ -5,13 +5,38 @@ using UnityEngine.SceneManagement;
 
 public class StartSceneScript : MonoBehaviour
 {
+    public GameObject settingsScreen;
+    private GameController controller;
+    private bool visible = false;
     public void StartGame()
     {
         SceneManager.LoadScene(1);
+
+        controller = (GameController)FindObjectOfType(typeof(GameController));
+        if (controller)
+        {
+            Debug.Log("GameController exists");
+        }
+        else
+        {
+            Debug.Log("GameController does not exists");
+        }
     }
 
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    public void ShowSettings()
+    {
+        if (visible)
+        {
+            settingsScreen.SetActive(false);
+        }else
+        {
+            settingsScreen.SetActive(true);
+        }
+        
     }
 }
