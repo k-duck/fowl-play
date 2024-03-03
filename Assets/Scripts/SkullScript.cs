f1a33e36caa263662196e8227189dce6600e4432
Max Ingleton <maxingleton@cunet.carleton.ca>
2024-03-03 16:33:10 -0500

    added cards

--- Assets/Scripts/SkullScript.cs
+++ Assets/Scripts/SkullScript.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class SkullScript : MonoBehaviour
 {
-    int CARD_INDEX = 6;
+    public GameObject keyCards;
+    public GameObject skulls;
+    public GameObject chests;
 
-    public Vector2 KeyCard;
+    int CARD_INDEX = 6;
 
     int TextureWidth = 1024;
     int TextureHeight = 1024;
@@ -14,10 +16,20 @@ public class SkullScript : MonoBehaviour
     int codeVal;
     List<int> codeValPrev = new List<int>();
 
+    public List<int> incorrectCards = new List<int> { 0, 1, 2, 3, 4, 5 }; //the cards that don't work
+    public List<int> RandCardOrder = new List<int> { 0, 1, 2, 3, 4, 5 }; //the cards that are assigned to a chest
+
+    public List<int> cardSymbol = new List<int>(); //stores symbol values for associated cards
+    public List<Transform> correctCards = new List<Transform> { }; //stores card order
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        RandomizeCards();
+
+        //RandomizeSkulls();
+
+
     }
 
     // Update is called once per frame
@@ -26,27 +38,77 @@ public class SkullScript : MonoBehaviour
         
     }
 
-    public Vector2 RandomCodeCoords(int x, int y)
+    public void RandomizeCards()
     {
-        //old code (just in case? idk)
-        /*
-        while (codeVal == codeValPrev || codeVal == KeyCard.y)
+        for(int i = 0; i < 4; i++)
         {
-            //takes input coordinates and picks a random column and row relative to them
-            int xRand = Random.Range(0, 8);
-            int yRand = Random.Range(0, 1);
-
-            x = x + (xRand * (TextureWidth / 10));
-            y = y - (yRand * (TextureHeight / 10));
-
-            //logs the number of the random symbol
-            codeVal = (xRand + 1) + (9 * yRand);
+            incorrectCards.RemoveAt(Random.Range(0, incorrectCards.Count));
+        }
+        for(int i = 0; i < incorrectCards.Count; i++)
+        {
+            RandCardOrder.Remove(incorrectCards[i]);
         }
 
-        codeValPrev = codeVal;
-        return new Vector2(x, y);
-        */
+        CARD_INDEX = 6;
+        int CHILD_INDEX = 2;
 
+        CHILD_INDEX = 1;
+
+        for (int i = 0; i < CARD_INDEX; i++)
+        {
+            Debug.Log("i: " + i); 
+            Mesh mesh = keyCards.gameObject.transform.GetChild(i).GetChild(CHILD_INDEX).GetComponent<MeshFilter>().mesh;
+
+            Vector3[] vertices = mesh.vertices;
+            Vector2[] uv = new Vector2[vertices.Length];
+            int[] triangles = mesh.triangles;
+
+            int CodeStartX = (TextureWidth / 20);
+            int CodeStartY = 430;
+            int CodeWidth = (TextureWidth / 10);
+            int CodeHeight = 164;
+
+            Vector2 Coords = RandomCodeCoords(CodeStartX, CodeStartY);            
+
+            //moving the uvs
+            uv[0] = ConvertPixelsToUVCoordinates((int)Coords.x, (int)Coords.y + CodeHeight, TextureWidth, TextureHeight);
+            uv[1] = ConvertPixelsToUVCoordinates((int)Coords.x + CodeWidth, (int)Coords.y + CodeHeight, TextureWidth, TextureHeight);
+            uv[2] = ConvertPixelsToUVCoordinates((int)Coords.x, (int)Coords.y, TextureWidth, TextureHeight);
+            uv[3] = ConvertPixelsToUVCoordinates((int)Coords.x + CodeWidth, (int)Coords.y, TextureWidth, TextureHeight);
+
+            //applying the defined attributes to the mesh
+            mesh.uv = uv;
+            keyCards.gameObject.transform.GetChild(i).GetChild(CHILD_INDEX).GetComponent<MeshFilter>().mesh = mesh;
+
+            if (incorrectCards.Contains(i) == false)
+            {
+                //pick random order for chest card locations
+                int randLoc = Random.Range(0, RandCardOrder.Count);
+
+                //changes the card name to be identifiable by the chests
+                string cardName = "chestCard_" + i;
+                keyCards.gameObject.transform.GetChild(RandCardOrder[randLoc]).name = cardName;
+                correctCards.Add(keyCards.gameObject.transform.GetChild(RandCardOrder[randLoc]));
+
+                //stores code value for card
+                cardSymbol.Add(codeVal);
+
+                //removes chest from location pool
+                RandCardOrder.RemoveAt(randLoc);
+            }            
+        }
+    }
+
+    public void RandomizeSkulls()
+    {
+        for(int i = 0; i < 4; i++)
+        {
+
+        }
+    }
+
+    public Vector2 RandomCodeCoords(int x, int y)
+    {        
         bool loop = true;
 
         int xOut = x;
@@ -62,8 +124,6 @@ public class SkullScript : MonoBehaviour
             xOut = x + (xRand * (TextureWidth / 10));
             yOut = y - (yRand * ((TextureHeight / 10) * 2));
 
-            //Debug.Log("xRand: " + xRand + ", yRand: " + yRand + ", x: " + x + ", y: " + y);
-
             //logs the number of the random symbol
             codeVal = (xRand + 1) + (9 * yRand);
 
@@ -73,17 +133,6 @@ public class SkullScript : MonoBehaviour
                 loop = false;
             }
         }
-
-        //output the list to console
-        /*
-        for (int i = 0; i < codeValPrev.Count; i++)
-        {
-            if (codeValPrev[i] == codeVal)
-                Debug.Log("Current Num " + i + ": " + codeVal);
-            Debug.Log("List Num " + i + ": " + codeValPrev[i].ToString());
-        }
-        */
-
         return new Vector2(xOut, yOut);
     }
 
